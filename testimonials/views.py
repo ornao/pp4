2025fe8045e9e4eaeb9a6ecf4d664e8a7c6e33a8
ornao <reynoldsorna@gmail.com>
2023-06-06 19:41:57 +0000

    create view code for testimonials

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Testimonials
 
-# Create your views here.
+
+class TestimonialsList(generic.ListView):
+    model = Testimonials()
+    queryset = Testimonials().objects.filter(status=1).order_by('date_created')
+    template_name = "index.html"
