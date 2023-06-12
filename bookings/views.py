c0e77b3b68a63ef0ede65a0ef85f5214e6c1ce91
ornao <reynoldsorna@gmail.com>
2023-06-12 20:16:48 +0000

    check create functionality of booking form is updating to database

--- bookings/views.py
+++ bookings/views.py
@@ -12,3 +12,6 @@ class BookingsCreate(generic.CreateView):
     model = Bookings
     template_name = "create_bookings.html"
     fields = "first_name", "last_name", "email", "check_in_date", "check_out_date"
+    
+
+# need to add booking successful page and url 
