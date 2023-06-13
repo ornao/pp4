71ca61924cd2f97e51690aeb620b57b34c997f86
ornao <reynoldsorna@gmail.com>
2023-06-13 18:50:11 +0000

    add success url so page diverts to view bookings page

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Bookings
+from django.urls import reverse, reverse_lazy
 
 
 class BookingsList(generic.ListView):
@@ -8,7 +9,10 @@ class BookingsList(generic.ListView):
     template_name = "read_bookings.html"
 
     def get_queryset(self):
-        return Bookings.objects.filter(user=self.request.user).order_by('-created_date')
+        if self.request.user.is_authenticated:
+            return Bookings.objects.filter(user=self.request.user).order_by('-created_date')
+        else:
+            return Bookings.objects.none()
 
 
 
@@ -16,6 +20,7 @@ class BookingsCreate(generic.CreateView):
     model = Bookings
     template_name = "create_bookings.html"
     fields = "first_name", "last_name", "email", "check_in_date", "check_out_date"
+    success_url = reverse_lazy("read_bookings")
     
 
 # need to add booking successful page and url 
