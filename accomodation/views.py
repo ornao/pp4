efedb2c3fb4e4bb1938d1b9a1d64a1a94db8c1f7
ornao <reynoldsorna@gmail.com>
2023-06-16 18:08:52 +0000

    create accomodation html and start to add to

--- accomodation/views.py
+++ accomodation/views.py
@@ -5,5 +5,4 @@ from .models import Accomodation
 
 class AccomodationList(generic.ListView):
     model = Accomodation
-    # queryset = Accomodation.objects.filter(status=1).order_by('availibility')
-    # template_name = "accomodation.html"
+    template_name = "accomodation.html"
