f2532a2808018145ec6cc7a0164f307957439a2c
ornao <reynoldsorna@gmail.com>
2023-07-02 19:56:42 +0000

    add form validation so only up to max guests can stay in accomodation

--- bookings/forms.py
+++ bookings/forms.py
@@ -6,18 +6,21 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 class BookingsForm(forms.ModelForm):
-    # django documentation
+    # django documentation form validation
     def clean(self):
         cleaned_data = super().clean()
         check_in_date = cleaned_data.get("check_in_date")
         check_out_date = cleaned_data.get("check_out_date")
+        num_guests = cleaned_data.get("num_guests")
+        accomodation_name = cleaned_data.get("accomodation_name")
 
         if check_in_date and check_out_date:
-            # Only do something if both fields are valid so far.
-            if check_in_date > check_out_date:
-                raise ValidationError(
-                    "Check in date cannot be after check date!"
-                )
+            if check_in_date > check_out_date:   
+                raise ValidationError("Check-in date cannot be after check-out date")
+
+        if num_guests and accomodation_name:
+            if num_guests >= 3 and accomodation_name not in ['pod cupla', 'honeymoon pod']:
+                raise ValidationError("This pod will not allow for more than 2 guests")
 
     class Meta:
         model = Bookings
--- glamping/settings.py
+++ glamping/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ornao-pp4-x7tgdbocpyn.ws-eu101.gitpod.io', 'glamping2023.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-ornao-pp4-x7tgdbocpyn.ws-eu101.gitpod.io', 'https://glamping2023.herokuapp.com/', 'localhost']
 
 
 # Application definition
