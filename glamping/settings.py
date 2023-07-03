243eeed14b7d2d33643dfb06d43073b6a8835a47
ornao <reynoldsorna@gmail.com>
2023-07-03 17:45:58 +0000

    add debug to false for production

--- glamping/settings.py
+++ glamping/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ornao-pp4-x7tgdbocpyn.ws-eu101.gitpod.io', 'glamping2023.herokuapp.com', 'localhost']
 
