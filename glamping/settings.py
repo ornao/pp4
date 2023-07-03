1874ddb8bc1f2577223e1ac13c80167bcb1367eb
ornao <reynoldsorna@gmail.com>
2023-07-03 16:28:21 +0000

    enter heroku domain to local hosts once again

--- glamping/settings.py
+++ glamping/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ornao-pp4-x7tgdbocpyn.ws-eu101.gitpod.io', '*', 'localhost']
+ALLOWED_HOSTS = ['8000-ornao-pp4-x7tgdbocpyn.ws-eu101.gitpod.io', 'https://glamping2023.herokuapp.com/', 'localhost']
 
 
 # Application definition
